9d523f856f94d4b387ffc2eb090d49ba91c2ee3a
Aaron W <aaronw23880@gmail.com>
2023-07-16 17:01:20 -0400

    cleaned up git commits

--- amber.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "xtensor/xarray.hpp"
-
-void say_hello(){
-    std::cout << "Hello, from amber!\n";
-}
--- build/CMakeFiles/amber.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for amber.
-# This may be replaced when dependencies are built.
